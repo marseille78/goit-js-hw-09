bf3a4175361471f46db549203f362c6c50f9ac5e
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-03-11 15:09:57 +0200

    Task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+let intervalId = null;
+const refs = {};
 
+document.addEventListener('DOMContentLoaded', () => {
+  refs.btnStart = document.querySelector('[data-start]');
+  refs.btnStop = document.querySelector('[data-stop]');
+
+  if (refs.btnStart) {
+    refs.btnStart.addEventListener('click', () => startChange((refs.btnStart, refs.btnStop)));
+  }
+
+  if (refs.btnStop) {
+    refs.btnStop.disabled = true;
+    refs.btnStop.addEventListener('click', stopChange);
+  }
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startChange() {
+  intervalId = setInterval(changeBg, 1000)
+}
+
+function stopChange() {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+
+function changeBg() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
