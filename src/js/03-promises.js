681e4707c22ddab9cf9c28b8bc24f7469d453ad9
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-03-12 22:56:44 +0200

    Task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+import { Notify } from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const refs = {};
+const opts = {};
+
+document.addEventListener('DOMContentLoaded', () => {
+  refs.form = document.querySelector('.form');
+
+  refs.form.addEventListener('submit', handleSubmit);
+});
+
+function handleSubmit(e) {
+  e.preventDefault();
+  gatherData(refs.form);
+  let delay = 0;
+
+  delay += opts.delay;
+
+  setTimeout(() => {
+    for (let i = 1; i <= opts.amount; i++) {
+      createPromise(i, delay)
+        .then(({ position, delay }) => {
+          Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        });
+      delay += opts.step;
+    }
+  }, delay);
+
+  refs.form.reset();
+}
+
+function gatherData(form) {
+  new FormData(form).forEach((value, key) => {
+    opts[key] = +value;
+  });
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const data = {position, delay};
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve(data);
+      }
+
+      reject(data);
+    }, delay);
+  });
 }
